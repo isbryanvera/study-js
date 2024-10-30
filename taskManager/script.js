96d1880b891fa9a24c7482d41515de0dc28043fa
Bryan Vera <brayanandresveragar@gmail.com>
2024-10-29 20:27:07 -0500

    :sparkles: Create persistence with localStorage

--- taskManager/script.js
+++ taskManager/script.js
@@ -1,13 +1,15 @@
 const taskForm = document.getElementById('task-form')
 const taskList = document.getElementById('task-list')
 const referenceTask = document.querySelector('#task')
+const taskInput = document.getElementById('task-input')
+
+loadTasks();
 
 
 taskForm.addEventListener('submit',(e) => {
     e.preventDefault()
-    const taskInput = document.getElementById('task-input')
     const taskValue = taskInput.value
-    createTask(taskValue)
+    createTask(taskValue,true)
     console.log(taskValue)
 })
 
@@ -24,10 +26,14 @@ taskList.addEventListener('click',(e) => {
     }
 })
 
-function createTask(value){
+function createTask(value,isNew){
     const task = referenceTask.cloneNode(true)
     task.childNodes[0].nodeValue = value
     taskList.append(task)
+
+    if(isNew) storeTaskInLocalStorage(value)
+    
+    taskInput.value=""
 }
 
 function deleteTask(task){
@@ -39,8 +45,28 @@ function deleteTask(task){
 function editTask(taskItem){
     const newtask = prompt('Edita la tarea',taskItem.firstChild.textContent)
     
-    if(newtask !== null) {
+    if(newtask !== '') {
         console.log(taskItem.firstChild)
         taskItem.firstChild.nodeValue = newtask
+        updateLocalStorage()
     }
+}
+
+function storeTaskInLocalStorage(task){
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(task)
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+}
+
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || []; 
+
+    tasks.forEach(task => {
+        createTask(task,false)
+    });
+}
+
+function updateLocalStorage(){
+    const tasks = Array.from(document.querySelectorAll('li')).map( (li) => li.firstChild.textContent)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
 }
